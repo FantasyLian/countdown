fc6e2d9d9a15991091761fe8ddafb06e1d0b3df3
Mr.cat <fantasy.lian@163.com>
2020-06-21 13:47:15 +0800

    add App

---
+++ App.vue
@@ -0,0 +1,28 @@
+<template>
+    <div id="app">
+        <h1>短信验证码</h1>
+		<count-down></count-down> 
+    </div>
+</template>
+<script>
+import CountDown from './components/CountDown.vue';
+export default {
+    name: 'app',
+    components: { CountDown },
+    data() {
+        return {
+            
+        }
+    }
+}
+</script>
+<style lang="css">
+    #app {
+        font-family: "Avenir", Helvetica, Arial, sans-serif;
+        -webkit-font-smoothing: antialiased;
+        -moz-osx-font-smoothing: grayscale;
+        text-align: center;
+        color: #2c3e50;
+        margin-top: 60px;
+    }
+</style>
