d6f5c6e35469182a28af7a6c21384cae657ca06b
Mr.cat <fantasy.lian@163.com>
2020-06-21 14:46:09 +0800

    add components

---
+++ components/countdown.vue
@@ -0,0 +1,40 @@
+<template>
+	<div>
+		<button type="button" v-show="show" @click="getCode">获取验证码</button>
+		<button type="button" v-show="!show">{{ count }} 秒</button>
+	</div>
+</template>
+<script>
+import axios from 'axios';
+export default {
+	name: 'countdown',
+	data() {
+		return {
+			show: true,
+			count: '',
+			timer: null
+		}
+	},
+	created() {
+		
+	},
+	methods: {
+		getCode() {
+			const TIME_COUNT = 60;
+			if(!this.timer) {
+				this.count = TIME_COUNT;
+				this.show = false;
+				this.timer = setInterval(() => {
+					if(this.count > 0 && this.count <= TIME_COUNT) {
+						this.count--;
+					} else {
+						this.show = true;
+						clearInterval(this.timer);
+						this.timer = null;
+					}
+				}, 1000);
+			}
+		}
+	}
+}
+</script>
