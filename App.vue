a63effc4af687f9f2e081ed966fc1a1dd2a6ef82
Mr.cat <fantasy.lian@163.com>
2020-06-23 00:28:28 +0800

    add countdown2

--- App.vue
+++ App.vue
@@ -2,13 +2,17 @@
     <div id="app">
         <h1>短信验证码</h1>
 		<count-down></count-down> 
+        <br>
+        <br>
+        <count-down-two></count-down-two> 
     </div>
 </template>
 <script>
 import CountDown from './components/CountDown.vue';
+import CountDownTwo from './components/CountDown2.vue';
 export default {
     name: 'app',
-    components: { CountDown },
+    components: { CountDown, CountDownTwo },
     data() {
         return {
             
