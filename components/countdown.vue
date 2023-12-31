83d3ceb104c4a40e87500acb1192f7991c8e2f03
Mr.cat <fantasy.lian@163.com>
2023-12-31 21:52:57 +0800

    格式化代码

--- components/countdown.vue
+++ components/countdown.vue
@@ -1,45 +1,44 @@
 <template>
-	<div>
-		<button type="button" v-show="show" @click="getCode">获取验证码</button>
-		<button type="button" v-show="!show">{{ count }} 秒</button>
-	</div>
+  <div>
+    <button type="button" v-show="show" @click="getCode">获取验证码</button>
+    <button type="button" v-show="!show">{{ count }} 秒</button>
+  </div>
 </template>
 <script>
-import axios from 'axios';
 export default {
-	name: 'countdown',
-	data() {
-		return {
-			show: true,
-			count: '',
-			timer: null
-		}
-	},
-	created() {
+  name: 'countdown',
+  data () {
+    return {
+      show: true,
+      count: '',
+      timer: null
+    }
+  },
+  created () {
 
-	},
-	destroyed() {
-		clearInterval(this.timer)
-		this.timer = null
-		console.log('定时器已销毁')
-	},
-	methods: {
-		getCode() {
-			const TIME_COUNT = 60;
-			if(!this.timer) {
-				this.count = TIME_COUNT;
-				this.show = false;
-				this.timer = setInterval(() => {
-					if(this.count > 0 && this.count <= TIME_COUNT) {
-						this.count--;
-					} else {
-						this.show = true;
-						clearInterval(this.timer);
-						this.timer = null;
-					}
-				}, 1000);
-			}
-		}
-	}
+  },
+  destroyed () {
+    clearInterval(this.timer)
+    this.timer = null
+    console.log('定时器已销毁')
+  },
+  methods: {
+    getCode () {
+      const TIME_COUNT = 60
+      if (!this.timer) {
+        this.count = TIME_COUNT
+        this.show = false
+        this.timer = setInterval(() => {
+          if (this.count > 0 && this.count <= TIME_COUNT) {
+            this.count--
+          } else {
+            this.show = true
+            clearInterval(this.timer)
+            this.timer = null
+          }
+        }, 1000)
+      }
+    }
+  }
 }
 </script>
--- components/countdown3.vue
+++ components/countdown3.vue
@@ -1,61 +1,61 @@
 <template>
-	<div class="countdown" @click="fn">
-		<a>{{ context }}</a>
-	</div>
+  <div class="countdown" @click="fn">
+    <a>{{ context }}</a>
+  </div>
 </template>
 <script>
 export default {
-	name: 'countDown',
-	props: ['deftext', 'second', 'fn'],
-	data () {
-		return {
-			time: 0,
-			timer: null,
-			canClick: true
-		}
-	},
-	destroyed () {
-		clearInterval(this.timer)
-		this.timer = null
-		console.log('定时器已销毁')
-	},
-	computed: {
-		context () {
-			return this.time > 0 ? this.time + '秒' : this.deftext
-		}
-	},
-	methods: {
-		countDown () {
-			if (!this.canClick) return
-			this.time = this.second
-			this.canClick = false
-			this.timer = setInterval(() => {
-				this.time--
-				if (this.time < 0) {
-				clearInterval(this.timer)
-				this.canClick = true
-				}
-			}, 1000)
-		},
-		stopCount () {
-			clearInterval(this.timer)
-			this.timer = null
-			console.log('计时器停止')
-		}
-	}
+  name: 'countDown',
+  props: ['deftext', 'second', 'fn'],
+  data () {
+    return {
+      time: 0,
+      timer: null,
+      canClick: true
+    }
+  },
+  destroyed () {
+    clearInterval(this.timer)
+    this.timer = null
+    console.log('定时器已销毁')
+  },
+  computed: {
+    context () {
+      return this.time > 0 ? this.time + '秒' : this.deftext
+    }
+  },
+  methods: {
+    countDown () {
+      if (!this.canClick) return
+      this.time = this.second
+      this.canClick = false
+      this.timer = setInterval(() => {
+        this.time--
+        if (this.time < 0) {
+          clearInterval(this.timer)
+          this.canClick = true
+        }
+      }, 1000)
+    },
+    stopCount () {
+      clearInterval(this.timer)
+      this.timer = null
+      console.log('计时器停止')
+    }
+  }
 }
 </script>
 <style lang="less">
 .countdown {
-	display: inline;
+  display: inline;
 
-	a {
-		width: 110px;
-		height: 32px;
-		font-size: 14px;
-		font-weight: 400;
-		color: rgba(24, 144, 255, 1);
-		line-height: 32px;
-	}
+  a {
+    width: 110px;
+    height: 32px;
+    font-size: 14px;
+    font-weight: 400;
+    color: rgba(24, 144, 255, 1);
+    line-height: 32px;
+  }
 }
 </style>
